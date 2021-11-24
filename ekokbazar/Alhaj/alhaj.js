b6ee20a18b2af8607c8a30bcbd9da95b86932800
Alhaj Hossen <82259128+alhajhossen335@users.noreply.github.com>
2021-11-24 11:10:11 +0600

    Delete alhaj.js

--- ekokbazar/Alhaj/alhaj.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "Test Google": function (browser){
-        browser.url("https://www.google.com/");
-        browser.waitForElementVisible("input[type=text]");
-        browser.setvalue("input[type=text]", "bangladesh");
-        browser.click("input[type=submit]");
-    }
-}
